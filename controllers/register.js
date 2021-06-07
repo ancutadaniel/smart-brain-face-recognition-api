b21d009caff52ba542937767c0f83d927f9e926e
daniel.ancuta@3ss.tv <-5}2nJrR%TxRb}V!>
2021-06-07 17:21:22 +0300

    update register

--- controllers/register.js
+++ controllers/register.js
@@ -7,36 +7,29 @@ const handleRegister = (req, res, db, bcrypt) => {
       .json("Incorect from submision. Please fill all fields");
   }
   const hash = bcrypt.hashSync(password);
-
   db.transaction((trx) => {
-    // trx - transaction
-    try {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
-        .into("login")
-        .returning("email")
-        .then((loginEmail) => {
-          return trx("users")
-            .returning("*") // after insert return
-            .insert({
-              email: loginEmail[0],
-              name: name,
-              joined: new Date(),
-            })
-            .then((user) => {
-              res.status(201).json(user[0]);
-            })
-            .catch((err) => res.status(400).json("Error to register"));
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    } catch {
-      res.status(400).json("Unable to register.");
-    }
-  });
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
 };
 
 module.exports = { handleRegister };
