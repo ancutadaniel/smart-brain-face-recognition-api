7e9a37cc430db4cd7f015b9156639c5b5161f96b
daniel.ancuta@3ss.tv <-5}2nJrR%TxRb}V!>
2021-06-07 15:44:57 +0300

    update database url

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "",
+    host: "postgresql-rugged-37570",
     database: "smart-brain",
   },
 });
